c9af77a7fd3ac688fda5c68d7694e84e7a8ae679
Akshitpatel25 <patelakshit1225@gmail.com>
2025-07-22 16:57:58 +0530

    add vercel.json file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://buslivebackend-dphfy.ondigitalocean.app:3001',  // Update with your backend URL
+        changeOrigin: true,
+        secure: true,  // Set to true if using HTTPS
+      },
+    },
+  },
+});
